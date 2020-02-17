a0d4a96bdb356a564f97b51cf6e85c279443bd7f
Alex Pobyvanets <alekseipobyvanets@gmail.com>
2020-02-17 20:28:46 +0200

    My 1rst commit

---
+++ praktic.js
@@ -0,0 +1,7 @@
+function fff (){
+var a = prompt("Введите число 22", "0")
+var anse = (a == 22) ? document.write("Вы ввели правельное число") : document.write("Вы ввели неправельное число");
+};
+console.log(fff)
+
+fff();
