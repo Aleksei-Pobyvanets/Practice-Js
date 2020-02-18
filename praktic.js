ac37f6284dc0f285f99c91aa0f3b799f19e0abbb
Alex Pobyvanets <alekseipobyvanets@gmail.com>
2020-02-18 20:48:17 +0200

    Short function

--- praktic.js
+++ praktic.js
@@ -3,5 +3,4 @@ var a = prompt("Введите число 22", "0")
 var anse = (a == 22) ? document.write("Вы ввели правельное число") : document.write("Вы ввели неправельное число");
 };
 console.log(fff)
-
-fff();
+//fff();
